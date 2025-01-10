a35aa33bb8f9b6956b3bb0f114b75e1b50ce4b41
lmfrgsn <lmfrgsn@me.com>
2025-01-10 23:04:09 +0000

    start to build it out

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
-export default async function (eleventyConfig) { };
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+export default async function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+};
 
 export const config = {
     dir: {
